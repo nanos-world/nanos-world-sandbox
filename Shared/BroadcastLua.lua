89b35046ed428c49ff77a1a324f8845313925bf5
Cheatoid <Cheatoid@users.noreply.github.com>
2026-01-08 04:04:33 +0100

    Formatting

--- Shared/BroadcastLua.lua
+++ Shared/BroadcastLua.lua
@@ -1,23 +1,21 @@
 local BROADCASTLUA = "BroadcastLua"
 
--- Server-side
 if Server then
-  function BroadcastLua(code)
-    return Events.BroadcastRemote(BROADCASTLUA, code)
-  end
+	function BroadcastLua(code)
+		return Events.BroadcastRemote(BROADCASTLUA, code)
+	end
 
-  function Player.SendLua(player, code)
-    return Events.CallRemote(BROADCASTLUA, player, code)
-  end
+	function Player.SendLua(player, code)
+		return Events.CallRemote(BROADCASTLUA, player, code)
+	end
 
-  Package.Export(BROADCASTLUA, BroadcastLua)
-  return
+	Package.Export(BROADCASTLUA, BroadcastLua)
+else
+	-- Client-side
+	Events.SubscribeRemote(BROADCASTLUA, function(code)
+		local fn = load(code, BROADCASTLUA, "t")
+		if type(fn) == "function" then
+			pcall(fn)
+		end
+	end)
 end
-
--- Client-side
-Events.SubscribeRemote(BROADCASTLUA, function(code)
-  local fn = load(code, BROADCASTLUA, "t")
-  if type(fn) == "function" then
-    pcall(fn)
-  end
-end)
